8f41ec9e6da4da7dde841670ae96d7baf1709079
STIVEN-J4 <65369672+STIVEN-J4@users.noreply.github.com>
2020-06-10 20:06:44 -0500

    UML

---
+++ UML1.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uml1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Julio jimenez
+ */
+public class UML1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner objeto = new Scanner(System.in);
+        System.out.println("Estudio experimental y predicción de rendimiento y emisión en un "
+                + "motor SI que utiliza combustible alternativo con red neuronal artificial:");
+        System.out.println("\n   Ingrese el objeto de la clase padre del paper:");
+        String clp =objeto.nextLine();
+
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atri1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atri2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 3 :");
+        String atri3 =objeto.nextLine();
+
+        String atri4 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicios de la clase padre del paper:");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servi1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servi2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 3 :");
+        String servi3 =objeto.nextLine();
+
+
+              System.out.println("\n   Ingrese el objeto de la clase hijo 1 del paper:");
+        String hijo1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atri11 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atri21 =objeto.nextLine();
+
+
+        System.out.println("\n   Ingrese Servicios la clase hijo 1 :");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servi11 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servi21 =objeto.nextLine();
+
+
+
+    System.out.println("\n   Ingrese el objeto de la clase hijo 2 del paper:");
+        String hijo2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atri12 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atri22 =objeto.nextLine();
+
+
+        System.out.println("\n   Ingrese Servicios la clase hijo 2:");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servi12 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servi22 =objeto.nextLine();
+
+
+
+
+
+
+
+
+
+    }
+
+}
